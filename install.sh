fb5e912e72d36fa632c4ce826b9bc1635909ae1d
SIDHU <80940722+deathshifter29@users.noreply.github.com>
2021-07-29 02:23:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ apt-get install wget
 apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 
-npm install github:adiwajshing/baileys
-npm audit fix
+npm install
+@adiwajshing/baileys
 
 echo "[*] All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
 echo "Updates : by Sidhu ser offc"
